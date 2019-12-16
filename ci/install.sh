8df272b9220153cfc981ee9adc2def6de0b1f0de
Effy Elden <git@effy.is>
2019-12-16 13:58:56 +1100

    test

--- ci/install.sh
+++ ci/install.sh
@@ -5,7 +5,6 @@ main() {
     if [ $TRAVIS_OS_NAME = linux ]; then
         target=x86_64-unknown-linux-musl
         sort=sort
-        sudo apt-get install alsa-utils
     else
         target=x86_64-apple-darwin
         sort=gsort  # for `sort --sort-version`, from brew's coreutils.
