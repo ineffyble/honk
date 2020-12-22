2b54ce213a167c8f3a5f5fc692bf139ca49c4eef
Effy Elden <git@effy.is>
2020-12-22 16:31:14 +1100

    fix scripts

--- ci/before_deploy.ps1
+++ ci/before_deploy.ps1
@@ -4,21 +4,21 @@
 $SRC_DIR = $pwd.Path
 $STAGE = [System.Guid]::NewGuid().ToString()
 
+$VERSION = "$env:APPVEYOR_REPO_TAG_NAME".replace('v', '')
+
 Set-Location $env:TEMP
 New-Item -Type Directory -Name $STAGE
 Set-Location $STAGE
 
 $ZIP = "$SRC_DIR\$($env:CRATE_NAME)-$($env:APPVEYOR_REPO_TAG_NAME)-$($env:TARGET).zip"
 
-Get-ChildItem -recurse "$SRC_DIR\target"
-
 # TODO Update this to package the right artifacts
 Copy-Item "$SRC_DIR\target\$($env:TARGET)\release\$($env:CRATE_NAME).exe" '.\'
-Copy-Item "$SRC_DIR\target\release\wix\$($env:CRATE_NAME)-$($env:APPVEYOR_REPO_TAG_NAME)-x86_64.exe" '.\'
-
 7z a "$ZIP" *
-
 Push-AppveyorArtifact "$ZIP"
+
+Copy-Item "$SRC_DIR\target\release\wix\$($env:CRATE_NAME)-$($VERSION)-x86_64.msi" '.\'
+Push-AppveyorArtifact "$($env:CRATE_NAME)-$($VERSION)-x86_64.msi"
 
 Remove-Item *.* -Force
 Set-Location ..
