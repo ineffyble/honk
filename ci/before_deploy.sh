ee78c03f06719260d1e576453beb79145187dbca
Effy Elden <git@effy.is>
2020-12-22 15:37:22 +1100

    Attempt to fix release scripts

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -18,7 +18,7 @@ main() {
     test -f Cargo.lock || cargo generate-lockfile
 
     # TODO Update this to build the artifacts that matter to you
-    cross rustc --bin honk --target $TARGET --release -- -C lto
+    cross rustc --bin honk --target $TARGET --release
 
     # TODO Update this to package the right artifacts
     cp target/$TARGET/release/honk $stage/
